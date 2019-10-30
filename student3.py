4a1daaa453b14d1ea29c780f20de3b59f55de297
SamuelRDuarte <samuelduarte10@ua.pt>
2019-10-30 20:01:51 +0000

    sair do livel se tiver powerup and enemies == 0

--- student3.py
+++ student3.py
@@ -28,6 +28,8 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
         positions = []
         history = []
         limite = 0
+        got_powerup = False
+        powerup = [0,0]
 
         while True:
             try:
@@ -43,6 +45,9 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
 
                 if previous_level != None and previous_lives != None:
                     # se morrer ou passar de nível faz reset às variáveis globais
+                    if previous_level != state['level']:
+                        got_powerup = False
+                        powerup = [0,0]
                     if previous_level != state['level'] or previous_lives != state['lives']:
                         print('RESET')
                         calc_hide_pos = False
@@ -52,8 +57,11 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
                         history = []
                         goal = []
 
+
                 my_pos = state['bomberman']
                 ways = get_possible_ways(mapa, my_pos)
+                if my_pos == powerup:
+                    got_powerup = True
 
                 print('ways: ', end='')
                 print(ways)
@@ -61,7 +69,7 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
                 # fuga recursiva
                 if state['bombs'] != [] and not calc_hide_pos:
                     print("calcurar hide pos")
-                    goal, calc_hide_pos = choose_hide_pos2(my_pos, state['bombs'][0], mapa, '', 0, 70, state['enemies'])
+                    goal, calc_hide_pos = choose_hide_pos2(my_pos, state['bombs'][0], mapa, '', 0, 60, state['enemies'])
                     print('my pos:', my_pos)
                     print(goal)
                     print('hide pos: ' + str(calc_hide_pos))
@@ -103,14 +111,16 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
                         # key = choose_move(my_pos,ways,state['powerups'][0][0])
                         key,positions = choose_key(mapa, ways, my_pos, positions, state['powerups'][0][0], True)
                         goal = state['powerups'][0][0]
+                        powerup = state['powerups'][0][0]
+
 
                     # ir para 'exit'
-                    elif state['walls'] == [] and state['enemies'] == [] and state['powerups'] == []:
+                    elif got_powerup and state['enemies'] == [] and state['exit'] != []:
                         print("going to exit")
                         key,positions = choose_key(mapa, ways, my_pos, positions, state['exit'], True)
                         goal = state['exit']
                         # key = choose_move(my_pos,ways,state['exit'])
-                    elif state['walls'] != [] and state['powerups'] == []:
+                    elif state['walls'] != []:
                         print("Escolher parede alvo...")
                         print('my' + str(my_pos))
 
@@ -157,8 +167,7 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
 
                 if state['enemies'] != [] and state['bombs'] == []:
                     ##17/10 - Fugir dos inimigos
-                    ballooms = []
-                    ballooms = [b for b in state['enemies'] if b['name'] == "Balloom"]
+                    ballooms = state['enemies']
                     ballooms.sort(key=lambda x: dist_to(my_pos, x['pos']))
 
                     if in_range(my_pos, 3, ballooms[0]['pos'], mapa):
@@ -177,6 +186,7 @@ async def agent_loop(server_address="localhost:8000", agent_name="student"):
                 previous_lives = state['lives']
                 previous_key = key
                 print('Sending key: ' + key + '\n\n')
+                print("got_powerup: ",got_powerup)
 
                 await websocket.send(
                     json.dumps({"cmd": "key", "key": key})
